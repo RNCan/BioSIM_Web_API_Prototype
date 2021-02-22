1e47fec3fd8d7c699601c07bd72892844981a4a8
donenrique <donenrique@6b887517-cfc8-4d89-aedc-37eae35974fe>
2021-02-22 18:07:24 +0000

    Improving packaging for easy installation

--- trunk/setup.py
+++ trunk/setup.py
@@ -18,5 +18,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["flask", "waitress", "itsdangerous", "jinja2", "markupsafe", "six", "werkzeug"],
+    install_requires=["flask", "waitress", "itsdangerous", "jinja2", "markupsafe", "six", "werkzeug", "paste"],
 )
